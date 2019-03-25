c4132598aa70af9442c9f8e1304be05479dd71ff
Ramanjaneya Reddy Karnati <ramanjaneya.reddy.karnati@macys.com>
2019-03-25 12:26:30 -0300

    Added useEffect hook example

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 export default function App() {
   const [count, setCount] = useState(0);
+  useEffect(()=>{
+    document.title=`pressed these ${count} time`
+  });
   return(
     <div>
       <p>You clicked on {count} times</p>
